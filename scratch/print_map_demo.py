30def3f0aae09b54642c5ca5ef7dc473e3ad4c36
Madeleine Callan <148394224+mcallan-star@users.noreply.github.com>
2026-03-15 19:35:14 +0000

    drop an old scratch file

--- scratch/print_map_demo.py
+++
@@ -1,7 +0,0 @@
-import os, sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import numpy as np
-from touch_sensing import render_map
-import numpy as np
-m = np.zeros((4,4),bool); m[1,1]=m[2,3]=True
-print(render_map(m))   # quick look
